418707c7067b47f774e08884fb136a364707be87
hardytee1 <143975733+hardytee1@users.noreply.github.com>
2024-05-01 11:53:21 +0700

    Update trigger_1.sql

--- Tugas_trigger/trigger_1.sql
+++ Tugas_trigger/trigger_1.sql
@@ -1,16 +1,7 @@
-CREATE OR REPLACE TRIGGER trg_update_sel_count
-AFTER INSERT ON Narapidana
-FOR EACH ROW
-BEGIN
-    DECLARE sel_number INT;
-
-    -- Mendapatkan nomor sel dari narapidana yang baru dimasukkan
-    SELECT Sel_NomorSel INTO sel_number
-    FROM Narapidana
-    WHERE IDNarapidana = NEW.IDNarapidana;
-
-    -- Update jumlah now di tabel Sel
+CREATE TRIGGER `trg_update_sel_count` AFTER INSERT ON `narapidana`
+ FOR EACH ROW BEGIN
+    -- Update jumlah now di tabel Sel berdasarkan nomor sel dari record baru
     UPDATE Sel
     SET JumlahNow = JumlahNow + 1
-    WHERE NomorSel = sel_number;
-END;
+    WHERE NomorSel = NEW.Sel_NomorSel;
+END
