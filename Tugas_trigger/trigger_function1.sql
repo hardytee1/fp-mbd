cb7de4a94479dd8f314ba30faa1a1878cc1d55c6
hardytee1 <143975733+hardytee1@users.noreply.github.com>
2024-05-01 11:52:59 +0700

    Update trigger_function1.sql

--- Tugas_trigger/trigger_function1.sql
+++ Tugas_trigger/trigger_function1.sql
@@ -1,15 +1,13 @@
-CREATE OR REPLACE TRIGGER trg_check_capacity
-BEFORE INSERT OF Sel_NomorSel ON Narapidana
-FOR EACH ROW
-BEGIN
+CREATE TRIGGER `trg_check_capacity` BEFORE INSERT ON `narapidana`
+ FOR EACH ROW BEGIN
     DECLARE v_check INT;
 
-    -- Memanggil fungsi checkCapacity dengan parameter NomorSel dari narapidana yang akan diupdate
-    SET v_check = checkCapacity(OLD.Sel_NomorSel);
+    -- Memanggil fungsi checkCapacity untuk NomorSel yang baru di-update
+    SET v_check = checkCapacity(NEW.Sel_NomorSel);
 
-    -- Jika checkCapacity mengembalikan nilai 0, tampilkan pesan error
+    -- Jika checkCapacity mengembalikan 0, berarti sel sudah penuh, sehingga kita berikan pesan error
     IF v_check = 0 THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Kapasitas sel sudah penuh.';
+        SET MESSAGE_TEXT = 'Kapasitas sel sudah penuh. Perbaruan tidak dapat dilakukan.';
     END IF;
-END;
+END
