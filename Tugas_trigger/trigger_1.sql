b8399e57f7de29d3ce3bd7660dd78cc24abc428c
hardytee1 <143975733+hardytee1@users.noreply.github.com>
2024-05-01 12:58:43 +0700

    Update trigger_1.sql

--- Tugas_trigger/trigger_1.sql
+++ Tugas_trigger/trigger_1.sql
@@ -1,7 +1,15 @@
-CREATE TRIGGER `trg_update_sel_count` AFTER INSERT ON `narapidana`
- FOR EACH ROW BEGIN
+DELIMITER $$
+
+-- Create the trigger
+CREATE TRIGGER trg_update_sel_count 
+AFTER INSERT ON Narapidana
+FOR EACH ROW 
+BEGIN
     -- Update jumlah now di tabel Sel berdasarkan nomor sel dari record baru
     UPDATE Sel
     SET JumlahNow = JumlahNow + 1
     WHERE NomorSel = NEW.Sel_NomorSel;
-END
+END$$
+
+-- Reset the delimiter back to the default semicolon
+DELIMITER ;
