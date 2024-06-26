e6451bb913bcfa4aba773ceafed2fe86f9ea42fe
farrasariffadhila <89232500+farrasariffadhila@users.noreply.github.com>
2024-06-26 14:31:17 +0700

    Update index_baru.sql

--- Tugas_Index/index_baru.sql
+++ Tugas_Index/index_baru.sql
@@ -2,9 +2,11 @@ CREATE INDEX idx_jenis_sel ON Sel(JenisSel);
 CREATE INDEX idx_tanggal_masuk ON narapidana(TanggalMasuk);
 CREATE INDEX pengunjung_name_idx ON pengunjung (Nama);
 CREATE INDEX penjara_nama_idx ON Penjara (NamaPenjara);
+CREATE INDEX fasilitas_nama_idx ON Fasilitas (NamaFasilitas);
 
 
 EXPLAIN SELECT * FROM Sel WHERE JenisSel = 'Tipe A';
 EXPLAIN SELECT * FROM narapidana WHERE TanggalMasuk = '1998-12-18'
 EXPLAIN SELECT IDPengunjung FROM pengunjung WHERE Nama = 'Adda Rourke'
 EXPLAIN SELECT IDPenjara FROM Penjara WHERE NamaPenjara = 'Rook Prison';
+EXPLAIN SELECT * FROM Fasilitas WHERE NamaFasilitas = 'Ruang Pelatihan Kerja';
