759136264251d77bd59bb138db5eaae446aae44d
Inventiner <46766058+Inventiner@users.noreply.github.com>
2024-06-26 14:50:55 +0700

    Update Dummy Data and CDM PDM

---
+++ Data CSV/FasilitasRehab Data.sql
@@ -0,0 +1,74 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.28-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.7.0.6850
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table fpmbd.fasilitasrehabilitas: ~0 rows (approximately)
+INSERT INTO `fasilitasrehabilitas` (`ProgramRehabilitasi_IDProgram`, `Fasilitas_IDFasilitas`) VALUES
+	(1, 11),
+	(2, 5),
+	(3, 26),
+	(4, 15),
+	(5, 8),
+	(6, 24),
+	(7, 18),
+	(8, 19),
+	(9, 2),
+	(10, 19),
+	(11, 25),
+	(12, 13),
+	(13, 22),
+	(14, 25),
+	(15, 8),
+	(16, 6),
+	(17, 4),
+	(18, 14),
+	(19, 8),
+	(20, 4),
+	(21, 13),
+	(22, 9),
+	(23, 8),
+	(24, 2),
+	(25, 18),
+	(26, 19),
+	(27, 26),
+	(28, 9),
+	(29, 30),
+	(30, 5),
+	(31, 22),
+	(32, 21),
+	(33, 19),
+	(34, 6),
+	(35, 9),
+	(36, 22),
+	(37, 24),
+	(38, 25),
+	(39, 26),
+	(40, 4),
+	(41, 17),
+	(42, 23),
+	(43, 2),
+	(44, 11),
+	(45, 29),
+	(46, 3),
+	(47, 20),
+	(48, 7),
+	(49, 15),
+	(50, 28);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
