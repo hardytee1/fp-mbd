5a789013866e5fe856a40c0822b9707346db5b73
Hardy Tee <143975733+hardytee1@users.noreply.github.com>
2024-06-26 15:18:18 +0700

    Update trigger_function2.sql

--- Tugas_trigger/trigger_function2.sql
+++ Tugas_trigger/trigger_function2.sql
@@ -1,1 +1,20 @@
+DELIMITER $$
+
+CREATE TRIGGER trg_check_capacity_penjara
+BEFORE INSERT ON Narapidana
+FOR EACH ROW
+BEGIN
+    DECLARE v_check INT;
+
+    -- Call the function to check capacity for the new Sel_NomorSel
+    SET v_check = checkCapacityPenjara(NEW.Sel_NomorSel);
+
+    -- If the function returns 0, the Penjara is full, and an error is raised
+    IF v_check = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Kapasitas penjara sudah penuh. Penambahan narapidana tidak dapat dilakukan.';
+    END IF;
+END$$
+
+DELIMITER ;
 
